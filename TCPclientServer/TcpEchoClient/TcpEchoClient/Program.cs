42300ac342e81d63675671549251075a15f55379
Foxman <ron.mikhlin7@gmail.com>
2015-11-08 19:00:38 +0200

    added TCP echo client and server

---
+++ TCPclientServer/TcpEchoClient/TcpEchoClient/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Sockets;
+
+namespace TcpEchoClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert input:");
+
+            string echoString = Console.ReadLine(); // get input
+            byte[] byteBuffer = Encoding.ASCII.GetBytes(echoString); //encode string
+
+            string serverIP = "172.16.1.1";
+            int serverPort = 7;
+
+            TcpClient client = null;
+            NetworkStream netStream = null;
+            try
+            {
+
+                client = new TcpClient(serverIP, serverPort);
+
+                Console.WriteLine("Connected to server... sending echo stream");
+
+                netStream = client.GetStream();
+
+                netStream.Write(byteBuffer, 0, byteBuffer.Length); //send encoded string to server
+
+                Console.WriteLine("sent {0} bytes to server...", byteBuffer.Length);
+
+                int totalBytesReceieved = 0; //total bytes received
+                int bytesReceived = 0; //bytes received in last read
+
+                // Receive the same string back from the server
+                while (totalBytesReceieved < byteBuffer.Length)
+                {
+                    if ((bytesReceived = netStream.Read(byteBuffer, totalBytesReceieved, byteBuffer.Length - totalBytesReceieved)) == 0)
+                    {
+                        Console.WriteLine("Connection closed  prematurely.");
+                        break;
+                    }
+                    totalBytesReceieved += bytesReceived;
+                }
+
+                Console.WriteLine("Received {0} bytes from server: {1}", totalBytesReceieved, Encoding.ASCII.GetString(byteBuffer, 0, totalBytesReceieved));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                netStream.Close();
+                client.Close();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TCPclientServer/TcpEchoServer/TcpEchoServer/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpEchoServer
+{
+    class Program
+    {
+        private const int BUFSIZE = 32; // Size of received buffer
+
+        static void Main(string[] args)
+        {
+            int serverPort = 7;
+            TcpListener listener = null;
+            try
+            {
+                // create a TcpListener to accept client connections
+                listener = new TcpListener(IPAddress.Any, serverPort);
+                listener.Start();
+            }
+            catch(SocketException se)
+            {
+                Console.WriteLine(se.ErrorCode + ": " + se.Message);
+                Environment.Exit(se.ErrorCode);
+            }
+
+            byte[] receiveBuffer = new Byte[BUFSIZE]; // receive buffer
+            int bytesReceived;                         // receive byte count
+
+            for (; ; ) //run forever, accepting and servicing connections
+            {
+                TcpClient client = null;
+                NetworkStream netStream = null;
+
+                try
+                {
+                    client = listener.AcceptTcpClient(); // get client connections
+                    netStream = client.GetStream();
+                    Console.Write("handling client - ");
+
+                    //Receive untill client closes connection, indicated by 0 return value
+                    int totalBytesEchoed = 0;
+
+                    while ((bytesReceived = netStream.Read(receiveBuffer, 0, receiveBuffer.Length)) > 0)
+                    {
+                        netStream.Write(receiveBuffer, 0, bytesReceived);
+                        totalBytesEchoed += bytesReceived;
+                    }
+
+                    Console.WriteLine("echoed {0} bytes", totalBytesEchoed);
+
+                    //Close the stream and socket. we are done with this client.
+                    netStream.Close();
+                    client.Close();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    netStream.Close();
+                }
+            }
+
+           
+
+        }
+    }
+}
